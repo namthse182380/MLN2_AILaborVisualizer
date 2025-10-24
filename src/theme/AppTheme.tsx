9124ebf77e1502e24fbd8d7c57c33c04bd0eac3b
HoangNam <hnamt04@gmail.com>
2025-10-24 15:22:56 +0700

    final v2

--- src/theme/AppTheme.tsx
+++ src/theme/AppTheme.tsx
@@ -1,3 +1,4 @@
+// src/theme/AppTheme.tsx
 import * as React from 'react';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 import type { ThemeOptions } from '@mui/material/styles';
@@ -23,12 +24,12 @@ export default function AppTheme(props: AppThemeProps) {
     return disableCustomTheme
       ? {}
       : createTheme({
-          // For more details about CSS variables configuration, see https://mui.com/material-ui/customization/css-theme-variables/configuration/
+          // --- FIX: Đồng bộ hóa cấu hình với theme gốc ---
           cssVariables: {
-            colorSchemeSelector: 'data-mui-color-scheme',
+            colorSchemeSelector: 'class', // Thay đổi từ 'data-mui-color-scheme'
             cssVarPrefix: 'template',
           },
-          colorSchemes, // Recently added in v6 for building light & dark mode app, see https://mui.com/material-ui/customization/palette/#color-schemes
+          colorSchemes,
           typography,
           shadows,
           shape,
